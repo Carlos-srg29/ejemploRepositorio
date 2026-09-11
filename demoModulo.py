ed4cd5a93c45996e283ea9e4e1ab3950475fbc1e
60672827-afk <60672827@certus.edu.pe>
2026-09-10 21:50:38 -0500

    libreria

--- demoModulo.py
+++ demoModulo.py
@@ -1,3 +1,9 @@
 import deteccionDeTemperatura
 test = deteccionDeTemperatura.detectarTemperatura(17,22)
 print (f"El resultado es: {test}")
+
+print(deteccionDeTemperatura.sumaDeNumeros(34,56))
+
+parametro = deteccionDeTemperatura.sumaDeNumeros(23,-86)
+otrotest=deteccionDeTemperatura.detectarTemperatura(parametro,-7)
+print(f"resultado final: {otrotest}")
--- deteccionDeTemperatura.py
+++ deteccionDeTemperatura.py
@@ -2,9 +2,16 @@
 #Creando una libreria
 
 def detectarTemperatura(lectura,consigna):
-    if lectura == consigna:
-        return True
-    else:
-    #Acciones si es falso.
-        return False
+    if lectura == 17:
+    #Acciones si es verdadero.
+        print("La temperatura es igual 17")
+        lectura=lectura+3
+        print(f"temperatura: {lectura}")
 
+#Implementar una funcion que sume 2 numeros, devuelve suma.
+def sumaDeNumeros(numero1, numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+resultado = sumaDeNumeros(34,56)
+print(f"La suma es: {resultado}") 
